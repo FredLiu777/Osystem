cfb5f78347a71985413ef4e58ad40fe9ff507c83
FredLiu <liulesheng777@gmail.com>
2020-07-28 15:57:13 +1200

    Initial commit

--- osystem-coupon/src/main/java/com/osystem/mall/osystemcoupon/OsystemCouponApplication.java
+++ osystem-coupon/src/main/java/com/osystem/mall/osystemcoupon/OsystemCouponApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class OsystemCouponApplication {
-
+//Coupon
     public static void main(String[] args) {
         SpringApplication.run(OsystemCouponApplication.class, args);
     }
