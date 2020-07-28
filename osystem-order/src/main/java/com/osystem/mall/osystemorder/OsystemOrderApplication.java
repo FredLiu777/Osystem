8f93fdfab0be7ba00d80b717d7cd80ce6dd5c5c7
FredLiu <liulesheng777@gmail.com>
2020-07-28 16:25:30 +1200

    Initial commit test

--- osystem-order/src/main/java/com/osystem/mall/osystemorder/OsystemOrderApplication.java
+++ osystem-order/src/main/java/com/osystem/mall/osystemorder/OsystemOrderApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class OsystemOrderApplication {
-
+//Order
     public static void main(String[] args) {
         SpringApplication.run(OsystemOrderApplication.class, args);
     }
